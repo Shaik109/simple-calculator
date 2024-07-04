d35b4fc4afc10bed6fa21339756824fd3cd1e354
Shaik109 <144472436+Shaik109@users.noreply.github.com>
2024-07-04 12:30:58 +0530

    simple-calculator website created by me

---
+++ index.js
@@ -0,0 +1,23 @@
+// script.js
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendCharacter(character) {
+    const display = document.getElementById('display');
+    display.value += character;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
